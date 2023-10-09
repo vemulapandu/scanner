b90338fa1b371eb9bcad61b7b40a7ce6a1dc1d8f
vemulapandu <mallik.prabhanjan@gmail.com>
2023-10-09 20:45:01 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        content = req.read()
+        requirements = content.split('\n')
+    return requirements
+
+setup(
+    name='scanner',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    entry_point=''''
+        [console_scripts]
+        scanner=scanner.__main__:main
+    '''
+)
